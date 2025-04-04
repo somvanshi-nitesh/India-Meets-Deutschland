2aa46e02588080363fea8e5a55aa6869370432ad
Nitesh <niteshsomvanshi.it@gmail.com>
2025-04-05 00:32:02 +0200

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>India Meets Deutschland</h1>
+      <p>Bridging Cultures, Celebrating Unity</p>
+      <p>
+        India Meets Deutschland is a cultural initiative to bring Indian and German communities together through events, art, and dialogue.
+      </p>
+    </div>
+  );
+}
