cffc2d5d285c027e8be7eb49045ad37065c5ce1f
Nitesh <niteshsomvanshi.it@gmail.com>
2025-04-05 00:32:40 +0200

    Create About.jsx

---
+++ src/pages/About.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>We are a nonprofit group promoting Indian and German cultural exchange through immersive experiences, community projects, and public events.</p>
+    </div>
+  );
+}
